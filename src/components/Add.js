9f8bc813f57e0a6245847c6bae45d53c3769edcb
Xiaoping-Li <narci2008@gmail.com>
2019-08-05 16:36:10 -0700

    Simple add app

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Form from './Form';
 
 class Add extends Component {
   render() {
     return (
-      <div className="App">
-       <h1>Add Function</h1>
-      </div>
+        <div>
+          <h1>Add Function</h1>
+          <Form operator='+'/>
+        </div>
     );
   }
 }
